28752dd708a2dc0f05bf329158a029bd4d01da5e
krk <keremkat@gmail.com>
2019-05-26 00:28:06 +0200

    Add empty parser.cpp

---
+++ include/parser.hpp
@@ -0,0 +1,14 @@
+#ifndef _PARSER_HPP_INCLUDED
+#define _PARSER_HPP_INCLUDED
+
+namespace car
+{
+namespace parser
+{
+class Parser
+{
+};
+} // namespace parser
+} // namespace car
+
+#endif _PARSER_HPP_INCLUDED
